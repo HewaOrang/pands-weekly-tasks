dc3cce392e199a8e929b7e71120bf60df0033dea
Hewa Orang <hewaorang@gmail.com>
2025-02-11 14:53:11 +0000

    banks.py

---
+++ banks.py
@@ -0,0 +1,20 @@
+# bank.py
+# This program reads in two amounts in cents and prints their sums out in euros
+# Author: Hewa Orang
+# Reference: I used ChatGPT to get the conversion 
+
+
+# This reads two values in cents
+amount1 = int(input("Enter amount 1(in cent):"))
+amount2 = int(input("Enter amount 2(in cent):"))
+
+# This sums the values in cents
+sum = (amount1 + amount2)
+
+# Convert to euros (1 euro = 100 cents)
+euros = sum / 100
+
+
+print('The sum of this is €' , euros)
+
+
