958335919b4c1166ad8deffae717c67713f2946e
Hewa Orang <hewaorang@gmail.com>
2025-04-21 16:46:33 +0100

    pands-weekly-tasks

--- es.py
+++ es.py
@@ -1,11 +1,23 @@
 # es.py
 # This program reads in a text file and outputs the number of e's it contains
 # Author: Hewa Orang
+# reference: https://docs.python.org/3/library/sys.html
 
+import sys  
+import os
 
+# # make sure the user provided a filepath as an argument to our script
+if len(sys.argv) < 2:
+    print("Error: Please provide filepath")
+    exit(1)
+# 
+FILENAME = sys.argv[1]
 
-FILENAME = "moby-dick1.txt"
+if not os.path.exists(FILENAME):
+    print("Error: filepath: {} doesn't exists!".format(FILENAME))
+    exit(1)
 
+# FILENAME = input("enter your file name")
 
 with open(FILENAME, 'r') as f:
     letter = f.read()
