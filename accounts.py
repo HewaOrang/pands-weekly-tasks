89e54a0045334ef4ce824bf6305c3ec5460ac175
Hewa Orang <hewaorang@gmail.com>
2025-04-26 12:46:22 +0100

    pands-weekly-tasks

--- accounts.py
+++ accounts.py
@@ -1,13 +1,18 @@
 # account.py
-# This program reads in a 10 chara. acc no. and 
+# This program reads in acc no. of any length and 
 # outputs the last 4 digits and the first 6 digits replaced with Xs 
 # Author: Hewa Orang
 # references: https://stackoverflow.com/questions/26921164/how-to-apply-a-custom-mask-into-a-string-of-digits-in-python
-#              
+#     https://docs.python.org/3/library/stdtypes.html#string-methods         
 
-accountNumber = (input ("Please enter a 10 digit account number:"))
-masked = "X" * 6
-print(f"{masked}{accountNumber[6:]}")
+# Prompt the user to input a 10-digit account number
+account_number = (input ("Please enter a 10 digit account number:"))
+
+if account_number.isdigit():   # Checks if the input contains only digits
+    masked_part = "X" * max(0, len(account_number) -4)  # Create the masked part "X" outputed for all digits except the last 4
+    print(f"{masked_part}{account_number[-4:]}") # prints the masked acc number
+else:
+    print(f"invalid input! Please enter a numeric account number.") # prints error message if the account number is not all digits
 
 
 
